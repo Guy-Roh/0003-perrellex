6be7d1e4b9bd8ea736b7917f13e24d8208199635
Guy Roh <lizgony@gmail.com>
2023-11-26 01:30:35 +0100

    image remap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React , {useState, useEffect} from 'react';
 import Hero from './components/Hero';
 import PortfolioSection from './components/PortfolioSection';
 import AboutSection from './components/AboutSection';
 import ContactSection from './components/ContactSection';
 
 import './App.scss';
-import ServicesSection from './components/ServicesSection';
 
 const App =()=> {
   return (
