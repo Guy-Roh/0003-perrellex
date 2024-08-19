91fa3f0bd696b9e95307c77206be01694e6fab31
Guy-Roh <lizgony@gmail.com>
2024-08-19 23:59:16 +0200

    update: made by footer

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
 
 const ContactSection = () => {
     // Each icon will lead to the desired platform
@@ -13,39 +13,51 @@ const ContactSection = () => {
     ];
 
     const handleClick = (listNumber: number) => {
-        window.open(socialLinks[listNumber], '_blank');
+        window.open(socialLinks[listNumber], "_blank");
     };
 
     const handleCopyToClipboard = (listNumber: number, attribute: string) => {
         navigator.clipboard.writeText(socialLinks[listNumber]);
-        return (
-            alert(attribute + " copied to clipboard!")
-        )
-    }
+        return alert(attribute + " copied to clipboard!");
+    };
 
     return (
         <div>
             <section id="CONTACT">
                 <h1>CONTACT</h1>
                 <p className="contact-field">
-                    <InstagramIcon onClick={() => handleClick(0)} style={{ cursor: 'pointer' }} />
+                    <InstagramIcon
+                        onClick={() => handleClick(0)}
+                        style={{ cursor: "pointer" }}
+                    />
                     <br />
-                    <YouTubeIcon onClick={() => handleClick(1)} style={{ cursor: 'pointer' }} />
+                    <YouTubeIcon
+                        onClick={() => handleClick(1)}
+                        style={{ cursor: "pointer" }}
+                    />
                     <br />
-                    <EmailIcon onClick={() => handleCopyToClipboard(2, "e-mail")} style={{ cursor: 'pointer' }} />
+                    <EmailIcon
+                        onClick={() => handleCopyToClipboard(2, "e-mail")}
+                        style={{ cursor: "pointer" }}
+                    />
                     <br />
                 </p>
             </section>
             <footer>
                 <p>© 2024 Perrellex</p>
-                <p>website made by<p onClick={
-                    () => window.open("https://www.instagram.com/studio_haedin/", '_blank')
-                } style={{ cursor: 'pointer' }
-                }>
-                Haedin</p></p>
+                <p
+                    onClick={() =>
+                        window.open(
+                            "https://www.instagram.com/studio_haedin/",
+                            "_blank"
+                        )
+                    }
+                    style={{ cursor: "pointer" }}
+                >
+                    Website made by Haedin
+                </p>
             </footer>
         </div>
-
     );
 };
 
